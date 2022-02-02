e8de042847c3bb7fd05ae21bd858648c51548458
Pearstopher <pearstopher@gmail.com>
2022-02-02 11:14:15 -0800

    added support for average and best values per trial. added silent mode to gradient_descent because quiet mode was too noisy

--- main.py
+++ main.py
@@ -24,11 +24,17 @@
 # pip install sympy
 import sympy as sp
 import random as rand
+import numpy as np
 
-LOOPS = 500
+LOOPS = 500  # default 500
+TRIALS = 10  # default 10
+STEPS = (0.1, 0.01, 0.001)  # default 0.1, 0.01, 0.001
 
 
-def gradient_descent(step_size, display="quiet"):
+# "silent"  does not print any output to the screen
+# "quiet"   only prints results of final iteration of each trial
+# "loud"    prints values after every single iteration
+def gradient_descent(step_size, display="loud"):
 
     # initialize symbols
     x, y = sp.symbols('x y')
@@ -54,28 +60,58 @@ def gradient_descent(step_size, display="quiet"):
     def step(value):
         # x_t = x_(t-1) - η∇f( x_(t-1) )
         for i in range(len(value)):
-            # global eta
+            # eta is just in scope
+            # I am substituting the value for x and y both times, out of confusion mostly
             value[i] = value[i] - eta * sp.N(del_f[i].subs(x, value[i]).subs(y, value[i]))
         return value
 
     # run gd for 500 steps
     for i in range(LOOPS):
-        if display != "quiet":
+        if display == "loud":
             print_val(v, i, "Iteration")
         v = step(v)
 
-    print_val(v, LOOPS, "Final Iteration")
+    if display != "silent":
+        print_val(v, LOOPS, "Final Iteration")
+    return v
 
 
 # basic little function for running the program
 def run():
-    for step in (0.1, 0.01, 0.001):
-        gradient_descent(step, "quiet")
+    results = np.empty((len(STEPS), TRIALS, 2))
+
+    for trial in range(TRIALS):
+        print("Trial " + str(trial + 1) + "... ")
+
+        for step in range(len(STEPS)):
+            values = gradient_descent(STEPS[step], "silent")
+            results[step][trial] = values
+
+    def distance(val):
+        # calculate distance from true minimum (3, -5)
+        a = abs(val[0] - 3)
+        b = abs(val[1] + 5)
+        return pow(a**2 + b**2, 0.5)
+
+    print("\nResults:\n")
+    for step in range(len(STEPS)):
+        total = [0, 0]
+        avg = [0, 0]
+        best = [0, 0]
+        for trial in range(TRIALS):
+            value = results[step][trial]
+            total += value
+            if distance(value) < distance(best):
+                best = value
+
+        avg[0] = total[0] / TRIALS
+        avg[1] = total[1] / TRIALS
+
+        print("Step size: " + str(STEPS[step]))
+        print("\t Average value: " + str(avg))
+        print("\t Best value: " + str(best))
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-
     run()
-
-    # todo: Report the best performance out of 10 trials for each of the different η value cases.
